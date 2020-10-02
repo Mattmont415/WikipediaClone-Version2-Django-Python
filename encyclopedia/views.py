0dfa59619e6065080759f8a6d93090e995104b7b
Matt Mont <mattmont415@gmail.com>
2020-10-01 20:40:23 -0700

    Now the wiki/Page works. Work on error reporting

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from markdown2 import Markdown
 
 from . import util
 
@@ -7,4 +9,16 @@ def index(request):
     return render(request, "encyclopedia/index.html", { 
         "entries": util.list_entries()
     })
+  
+# def link(request, name):
+#     return render(request, f"entries/{name}.md")
+
+def entry(request, title):
+  theEntry = util.get_entry(title)
+  markdowner = Markdown()
+  return render(request, "encyclopedia/index.html", {
+    "title": markdowner.convert(theEntry)
+  })
+
+
 
--- entries/Django.md
+++ entries/Django.md
@@ -1,3 +1,5 @@
 # Django
 
 Django is a web framework written using [Python](/wiki/Python) that allows for the design of web applications that generate [HTML](/wiki/HTML) dynamically.
+
+Also, even though "Django" starts with the letter, 'D', it is silent, and therefore the pronunciation of the word is simply 'jango'
